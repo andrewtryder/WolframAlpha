0d2211f0de15642f75c6ee372c756f9118059dd3
spline <reticulatingspline@github.com>
2014-09-29 11:13:20 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,5 +10,9 @@ from supybot.test import *
 class WolframAlphaTestCase(PluginTestCase):
     plugins = ('WolframAlpha',)
 
+    def testWolframAlpha(self):
+        conf.supybot.plugins.WolframAlpha.apiKey.setValue('VV6W6X-R7389R5AU5')
+        conf.supybot.plugins.WolframAlpha.disableANSI.setValue('True')
+        self.assertResponse('wolframalpha --shortest 2+2', '2+2 :: 4')
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
