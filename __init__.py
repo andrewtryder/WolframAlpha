0fe34ff04bf3eb478c42e7232592de0a70de146a
spline <reticulatingspline@github.com>
2014-10-16 10:10:06 -0400

    Update init.

--- __init__.py
+++ __init__.py
@@ -18,19 +18,21 @@ import supybot.world as world
 __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = 'reticulatingspline'
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/WolframAlpha/download'
+__url__ = 'https://github.com/reticulatingspline/WolframAlpha' # 'http://supybot.com/Members/yourname/WolframAlpha/download'
 
-import config
-import plugin
+from . import config
+from . import plugin
+from imp import reload
+# In case we're being reloaded.
 reload(config)
-reload(plugin) # In case we're being reloaded.
+reload(plugin)
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
